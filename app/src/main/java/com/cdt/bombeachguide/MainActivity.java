07f0781324171223f6001395eda18252991bca02
Bui Minh Trang <trangminie@gmail.com>
2016-05-17 22:27:56 +0700

    add main activity and navigation header

--- app/src/main/java/com/cdt/bombeachguide/MainActivity.java
+++ app/src/main/java/com/cdt/bombeachguide/MainActivity.java
@@ -1,52 +1,113 @@
 package com.cdt.bombeachguide;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.cdt.bombeachguide.adapter.NavDrawerListAdapter;
+import com.cdt.bombeachguide.pojo.NavDrawerItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle drawerToggle;
+    private NavDrawerListAdapter mAdapter;
+
+    // slide menu items
+    private String[] navMenuTitles = {"Classic landscape", "Classic portrait", "Classic square", "Amazing frame"};
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        createActionBar();
+        initDrawer();
+        if (savedInstanceState == null) {
+            displayView(0);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void createActionBar(){
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initDrawer(){
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,mToolbar,R.string.drawer_open,R.string.drawer_close){
+            public void onDrawerClosed(View view) {
+
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                drawerToggle.setDrawerIndicatorEnabled(true);
+            }
+        };
+        mDrawerLayout.setDrawerListener(drawerToggle);
+
+        TypedArray navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);
+        ViewGroup header = (ViewGroup) getLayoutInflater().inflate(R.layout.header_navigation, mDrawerList,
+                false);
+        mDrawerList.addHeaderView(header);
+        ArrayList<NavDrawerItem> navDrawerItems = new ArrayList<NavDrawerItem>();
+        // Home
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
+        // Blocks & Item
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
+        // Mobs
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
+        // Youtube
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1)));
+
+
+        // Recycle the typed array
+        navMenuIcons.recycle();
+
+        mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
+
+        // setting the nav drawer list adapter
+        mAdapter = new NavDrawerListAdapter(getApplicationContext(),
+                navDrawerItems);
+        mDrawerList.setAdapter(mAdapter);
+    }
+
+    /**
+     * Slide menu item click listener
+     * */
+    private class SlideMenuClickListener implements
+            ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                                long id) {
+            // display view for selected nav drawer item
+            displayView(position);
+        }
+    }
+
+    /**
+     * Diplaying fragment view for selected nav drawer list item
+     * */
+    private void displayView(int position) {
+
     }
 }
---
+++ app/src/main/java/com/cdt/bombeachguide/adapter/NavDrawerListAdapter.java
@@ -0,0 +1,62 @@
+package com.cdt.bombeachguide.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cdt.bombeachguide.R;
+import com.cdt.bombeachguide.pojo.NavDrawerItem;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Trang on 5/17/2016.
+ */
+public class NavDrawerListAdapter extends BaseAdapter{
+    private Context context;
+    private ArrayList<NavDrawerItem> navDrawerItems;
+
+    public NavDrawerListAdapter(Context context, ArrayList<NavDrawerItem> navDrawerItems){
+        this.context = context;
+        this.navDrawerItems = navDrawerItems;
+    }
+
+    @Override
+    public int getCount() {
+        return navDrawerItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return navDrawerItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater mInflater = (LayoutInflater)
+                    context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+        }
+
+        ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
+        TextView txtTitle = (TextView) convertView.findViewById(R.id.title);
+        TextView txtCount = (TextView) convertView.findViewById(R.id.counter);
+
+        imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
+        txtTitle.setText(navDrawerItems.get(position).getTitle());
+
+
+        return convertView;
+    }
+}
