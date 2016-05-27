3ec77c4309af6380c6b34394ec74e75d4b4339ed
thanhcao90 <thanhcao90@gmail.com>
2016-05-27 20:37:56 +0700

    add code choose videos

--- app/src/main/java/com/cdt/bombeachguide/fragment/MainFragment.java
+++ app/src/main/java/com/cdt/bombeachguide/fragment/MainFragment.java
@@ -1,5 +1,6 @@
 package com.cdt.bombeachguide.fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.cdt.bombeachguide.R;
 import com.cdt.bombeachguide.VideoActivity;
@@ -16,25 +18,36 @@ import com.cdt.bombeachguide.VideoActivity;
  * Created by Trang on 5/21/2016.
  */
 public class MainFragment extends Fragment {
+    private Context context;
 
     public static MainFragment newInstance(){
         MainFragment fragment = new MainFragment();
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.main_fragment, container, false);
-        Button btnVideo = (Button) rootView.findViewById(R.id.btn_video);
-        btnVideo.setOnClickListener(new View.OnClickListener() {
+        ImageView img_videos = (ImageView)rootView.findViewById(R.id.img_videos);
+        ImageView img_troops = (ImageView)rootView.findViewById(R.id.img_troops);
+        ImageView img_tuong = (ImageView)rootView.findViewById(R.id.img_tuong);
+        img_videos.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = VideoActivity.newIntent(getContext());
-                startActivity(intent);
-
+            public void onClick(View v) {
+                Intent videosIntent = VideoActivity.newIntent(getActivity());
+                startActivity(videosIntent);
             }
         });
+
+
+
         return rootView;
     }
 }
